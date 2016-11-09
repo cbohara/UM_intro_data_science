a4fa33e1deed842b073eca783c50f982149be2e8
Charlie O'Hara <charlie.b.ohara@gmail.com>
2016-11-08 17:48:54 -0800

    user id challenge

--- Week1/list_comp.py
+++ Week1/list_comp.py
@@ -5,7 +5,6 @@ for number in range(0, 1000):
 
 # list comprehension provides readability and performance benefits
 my_list = [number for number in range(0,1000) if number % 2 == 0]
-print(my_list)
 
 # convert function into list comprehension
 def times_tables():
@@ -16,3 +15,11 @@ def times_tables():
     return times
 
 print(times_tables() == [i * j for i in range(10) for j in range(10)])
+
+# all user ids are 2 lower-case letters followed by 2 numbers
+# write a list comprehension for all possible user id
+lowercase = 'abcdefghijklmnopqrstuvwxyz'
+digits = '0123456789'
+
+answer = [one_char + two_char + one_num + two_num for one_char in lowercase
+    for two_char in lowercase for one_num in digits for two_num in digits]
