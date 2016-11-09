836d89e6feefd60298c3592f0d3bd243a71e485f
Charlie O'Hara <charlie.b.ohara@gmail.com>
2016-11-08 17:40:25 -0800

    data files

---
+++ Week1/user_id.py
@@ -0,0 +1,6 @@
+# all user ids are 2 lower-case letters followed by 2 numbers
+# write a list comprehension for all possible user id
+lowercase = 'abcdefghijklmnopqrstuvwxyz'
+digits = '0123456789'
+
+answer = []
